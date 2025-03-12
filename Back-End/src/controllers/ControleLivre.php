e458ceef3ff114dd7003fe2d52d34f770c71ffaa
RozerinIsbir <rozerinisbir27@gmail.com>
2025-03-11 23:37:25 -0400

    Controle livre presque finit et Controle utilisateur commencé

--- Back-End/src/controllers/ControleLivre.php
+++ Back-End/src/controllers/ControleLivre.php
@@ -11,7 +11,7 @@ class ControleLivre{
         // header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
         try {
-            $requete = $pdo -> prepare('SELECT l.id_livre, l.image, l.titre, l.description, l.style, l.style, l.date_parution, a.id_auteur, a.nom AS nom_auteur, a.date_naissance, a.description AS description_auteur 
+            $requete = $pdo -> prepare('SELECT l.id_livre, l.image, l.titre, l.description, l.style, l.date_parution, a.id_auteur, a.nom AS nom_auteur, a.date_naissance, a.description AS description_auteur 
             FROM livre l
             INNER JOIN auteur a ON l.auteur_id = a.id_auteur
             WHERE l.id_livre = :id');
@@ -32,7 +32,7 @@ class ControleLivre{
         header('Content-Type: application/json; charset=utf-8'); 
 
         try {
-            $requete = $pdo -> prepare('SELECT l.image, l,titre, l.style a.nom AS nom_auteur 
+            $requete = $pdo -> prepare('SELECT l.id_livre, l.image, l.titre, l.description, l.style, l.date_parution, a.id_auteur, a.nom AS nom_auteur 
             FROM livre l
             INNER JOIN auteur a ON l.auteur_id = a.id_auteur
             ');
@@ -52,16 +52,29 @@ class ControleLivre{
         header('Content-Type: application/json; charset=utf-8');  
 
         // Récupérer les paramètres de filtres via les query parameters
-
+        $style = isset($_GET['style']) ? $_GET['style'] : null;
+        $langue = isset($_GET['langue']) ? $_GET['langue'] : null;
 
         try {
-           $sql = ('');
-           $param = [];
+            $sql = ('SELECT SELECT l.id_livre, l.image, l.titre, l.description, l.style, l.date_parution, a.id_auteur, a.nom AS nom_auteur 
+            FROM livre l
+            INNER JOIN auteur a ON l.auteur_id = a.id_auteur
+            ');
+            $param = [];
 
-           $requete = $pdo->prepare($sql);
-           $requete->execute($sql);
-           $livre = $requete->fetchAll();
-           echo json_encode($livre);
+            if (!empty($style) && $style !== "Tous") {
+                $query .= ('AND style.name = :style');
+                $params['style'] = $style;
+            }
+            if (!empty($langue) && $langue !== "Tous") {
+                $query .= ('AND langue.name = :langue');
+                $params['langue'] = $langue;
+            }
+
+            $requete = $pdo->prepare($sql);
+            $requete->execute($sql);
+            $livre = $requete->fetchAll();
+            echo json_encode($livre);
         } catch (PDOException $e) {
             http_response_code(500);
             echo json_encode(['error' => $e->getMessage()]);
@@ -75,13 +88,24 @@ class ControleLivre{
         header('Content-Type: application/json; charset=utf-8');  
 
         $data = json_decode(file_get_contents('php://input'), true);
+        if (!isset($data['image'], $data['titre'], $data['auteur_id'], $data['description'], 
+            $data['style'],$data['date_parution'])) {
+            http_response_code(400);
+            echo json_encode(['error' => 'Données incomplètes']);
+            exit;
+        }
 
         try {
             $sql = ('INSERT INTO livre (image, titre, auteur_id, description, style, date_parution) 
             VALUES (:image, :titre, :auteur_id, :description, :style, :date_parution)');
             $requete = $pdo->prepare($sql);
             $requete->execute([
-
+                ':image' => $data['image'] ?? null,
+                ':titre' => $data['titre'] ?? null,
+                ':auteur_id' => $data['auteur_id'] ?? null,
+                ':description' => $data['description'] ?? null,
+                ':style' => $data['style'] ?? null,
+                ':date_parution' => $data['date_parution'] ?? null,
             ]);
             echo json_encode(['success' => true, 'message' => 'Livre ajouté avec succès']);
         } catch (PDOException $e) {
--- Back-End/src/controllers/ControleUtilisateur.php
+++ Back-End/src/controllers/ControleUtilisateur.php
@@ -9,10 +9,33 @@ class ControleUtilisateur{
         header('Access-Control-Allow-Origin: *');  
         header('Content-Type: application/json; charset=utf-8'); 
 
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if(!isset($data['nom'], $data['prenom'], $data['nomUtilisateur'], $data['motDePasse'])){
+            http_response_code(400);
+            echo json_encode(['error' => 'Données incomplètes']);
+            exit;
+        }
+
+        // $nom = $data['nom'];
+        // $prenom = $data['prenom'];
+        $nomUtilisateur = $data['nomUtilisateur'];
+        $motDePasse = password_hash($data['motDePasse'], PASSWORD_BCRYPT);
+        $type = 0; // Utilisateur est un client (0 = faux)
         try {
-           
+            $requete = $pdo->prepare('INSERT INTO Utilisateur (nomUtilisateur, motDePasse, type)
+            VALUES (:nomUtilisateur, :motDePasse, :type)
+            ');
+            $requete -> execute([
+                    ':nomUtilisateur' => $data['nomUtilisateur'] ?? null,
+                    ':motDePasse' => $data['motDePasse'] ?? null,
+                    ':type' => $data['type'] ?? null,
+            ]);
+            http_response_code(201);
+            echo json_encode(["message" => "Client enregistré avec succès"]);
         } catch (PDOException $e) {
-           
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);           
         }
     }
     public static function userLogin() {
